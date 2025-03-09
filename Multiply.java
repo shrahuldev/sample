bfc7bb60b145ba3e34081f9ca9aa80dc165ebe31
Rahul Sir <developer.codewriter@gmail.com>
2025-03-09 20:53:43 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,15 @@
+class Multiply {
+  public static void main(String[] args) {
+    int x, y, sum;
+    Scanner myObj = new Scanner(System.in);
+    System.out.println("Type a number:");
+    x = myObj.nextInt();
+
+    System.out.println("Type another number:");
+    y = myObj.nextInt();
+
+    sum = x + y;
+    System.out.println("Sum is: " + sum);
+    System.out.println("Demo Message");
+  }
+} 
